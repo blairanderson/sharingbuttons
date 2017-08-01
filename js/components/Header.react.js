47204082f19b94ba1323b7f271ac568fca069f0e
Blair Anderson <Blair81@gmail.com>
2017-08-01 10:29:12 -0700

    update links in the build

--- js/components/Header.react.js
+++ js/components/Header.react.js
@@ -1,10 +1,13 @@
 var Header = React.createClass({
   render: function() {
-    return(
+    return (
       <div className="header">
         <div className="header__wrapper">
-          <h1 className="header__title">Sharingbuttons.io</h1>
-          <h2 className="header__tagline">Super fast and easy Social Media Sharing Buttons. No JavaScript. No tracking.</h2>
+          <h1 className="header__title">SharingButtons</h1>
+          <h2 className="header__tagline">
+            Super fast and easy Social Media Sharing Buttons. No JavaScript. No
+            tracking.
+          </h2>
         </div>
       </div>
     );
--- makewebpackconfig.js
+++ makewebpackconfig.js
@@ -15,8 +15,10 @@ module.exports = function(options) {
 
     cssLoaders = ['file-loader?name=[path][name].[ext]', 'postcss-loader'];
     // Plugins
-    plugins = [// Plugins for Webpack
-      new webpack.optimize.UglifyJsPlugin({ // Optimize the JavaScript...
+    plugins = [
+      // Plugins for Webpack
+      new webpack.optimize.UglifyJsPlugin({
+        // Optimize the JavaScript...
         debug: true,
         compress: {
           warnings: false // ...but do not show warnings in the console (there is a lot of them)
@@ -27,7 +29,8 @@ module.exports = function(options) {
         debug: true,
         template: 'index.html', // Move the index.html file...
         filename: 'index.html',
-        minify: { // Minifying it while it is parsed using the following, self–explanatory options
+        minify: {
+          // Minifying it while it is parsed using the following, self–explanatory options
           removeComments: true,
           collapseWhitespace: true,
           removeRedundantAttributes: true,
@@ -42,69 +45,77 @@ module.exports = function(options) {
       })
     ];
 
-  // If app is in development
+    // If app is in development
   } else {
     // Entry
     entry = [
-      "webpack-dev-server/client?http://0.0.0.0:3000", // Needed for hot reloading
-      "webpack/hot/only-dev-server", // See above
+      'webpack-dev-server/client?http://0.0.0.0:3000', // Needed for hot reloading
+      'webpack/hot/only-dev-server', // See above
       path.resolve(__dirname, 'js/app.js') // Start with js/app.js...
     ];
     cssLoaders = ['style-loader', 'css-loader', 'postcss-loader'];
     // Only plugin is the hot module replacement plugin
     plugins = [
       new webpack.HotModuleReplacementPlugin() // Make hot loading work
-    ]
+    ];
   }
 
   return {
     entry: entry,
-    output: { // Compile into js/build.js
-      path: './build',
-      filename: "js/bundle.js"
+    output: {
+      // Compile into js/build.js
+      path: './docs',
+      filename: 'js/bundle.js'
     },
     debug: true,
     module: {
-      loaders: [{
+      loaders: [
+        {
           test: /\.js$/, // Transform all .js files required somewhere within an entry point...
           loader: 'babel', // ...with the specified loaders...
           exclude: path.join(__dirname, '/node_modules/') // ...except for the node_modules folder.
-        }, {
-          test:   /\.css$/, // Transform all .css files required somewhere within an entry point...
+        },
+        {
+          test: /\.css$/, // Transform all .css files required somewhere within an entry point...
           loaders: cssLoaders // ...with PostCSS
-        }, {
+        },
+        {
           test: /\.jpe?g$|\.gif$|\.png$/i,
-          loader: "file-loader?name=[path][name].[ext]"
+          loader: 'file-loader?name=[path][name].[ext]'
         }
       ]
     },
     plugins: plugins,
     postcss: function() {
       return [
-        require('postcss-import')({ // Import all the css files...
-          onImport: function (files) {
-              files.forEach(this.addDependency); // ...and add dependecies from the main.css files to the other css files...
+        require('postcss-import')({
+          // Import all the css files...
+          onImport: function(files) {
+            files.forEach(this.addDependency); // ...and add dependecies from the main.css files to the other css files...
           }.bind(this) // ...so they get hot–reloaded when something changes...
         }),
         require('postcss-simple-vars')(), // ...then replace the variables...
         require('postcss-focus')(), // ...add a :focus to ever :hover...
-        require('autoprefixer-core')({ // ...and add vendor prefixes...
+        require('autoprefixer-core')({
+          // ...and add vendor prefixes...
           browsers: ['last 2 versions', 'IE > 8', 'iOS > 5'] // ...supporting the last 2 major browser versions and IE 8 and up...
         }),
-        require('cssnano')({ // ...and minify the result.
+        require('cssnano')({
+          // ...and minify the result.
           autoprefixer: false, // Don't run autoprefixer since we've already done that...
           comments: {
             removeAll: true // ...and remove all comments, even those marked important.
           }
         }),
-        require('postcss-reporter')({ // This plugin makes sure we get warnings in the console
+        require('postcss-reporter')({
+          // This plugin makes sure we get warnings in the console
           clearMessages: true
         })
       ];
     },
-    target: "web", // Make web variables accessible to webpack, e.g. window
+    target: 'web', // Make web variables accessible to webpack, e.g. window
     debug: true,
     stats: false, // Don't show stats in the console
     progress: true
-  }
-}
+  };
+};
